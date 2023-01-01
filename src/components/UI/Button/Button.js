c14a27745dc4cdfe6c3f6908e2070bc8542ad35d
Pascal Schaffner <pascal.schaffner@hotmail.com>
2023-01-01 17:57:47 +0100

    Course 6 abgeschlossen mit Styled Components Implementation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
+// Styled Components ersetzt Button.css komplett und erzeugt on the fly unique Class Names auf den button, so dass der Style 100% auf diesen Button gekapselt ist.
+// TODO: Man kann VS Code so konfigurieren, dass der CSS Code korrekte syntax highlighting hat, statt als String anzuzeigen.
+const Button = styled.button`
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  // Condition wenn die Breite über 768 ist, wird folgender Style angewandt
+  @media (min-width: 768px) {
+    width: auto;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+`;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
